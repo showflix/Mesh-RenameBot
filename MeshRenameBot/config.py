7a3fbfe06c29c6f8b1962dcc68d7260e74bb5d44
Show Flix <83741010+showflix@users.noreply.github.com>
2024-06-14 20:53:01 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,15 +4,15 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "mongodb+srv://showflixuser:showflixuser@cluster0.mqv9zew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"]
+        API_HASH = [str, "a8d3503be0455c779a17193e48cab451"]
+        API_ID = [int, 14959925]
+        BOT_TOKEN = [str, "7174676764:AAFJGZBLpJCMwh19P509WZ-4o0w1CJR38t4"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
-        SLEEP_SECS = [int, 10]
-        IS_MONGO = [bool, False]
+        SLEEP_SECS = [int, 5]
+        IS_MONGO = [bool, True]
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
@@ -20,8 +20,8 @@ except ImportError:
         OWNER_ID = [int, 0]
 
         # Public username url or invite link of private chat
-        FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN = [str,"https://t.me/showflix_movie"]
+        FORCEJOIN_ID = [int,-1001792757301]
 
         TRACE_CHANNEL = [int, 0]
 
